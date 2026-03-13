88a27e2727bc836db57f46c3fbe657e6129c9b80
andron2304 <andriyopanasiuk2008@gmail.com>
2026-03-13 22:49:48 +0200

    board

--- ElectronicQueue/obj/Debug/net10.0/ElectronicQueue.AssemblyInfo.cs
+++ ElectronicQueue/obj/Debug/net10.0/ElectronicQueue.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ElectronicQueue")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c826a6ff7c8771af82aa8e060955730c83b8cf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+229ee3c51f110641122a8efccaedb1ab60aacff6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ElectronicQueue")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ElectronicQueue")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
